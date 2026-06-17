afa52f61722c32cf56f5b255d3418f075e5e2a37
shehan183 <IT26101226@my.sliit.lk>
2026-06-17 16:24:20 +0530

    Add files via upload

---
+++ Q2/Lab2Q2.java
@@ -0,0 +1,20 @@
+public class Lab2Q2 {
+
+	public static void main(String[] args) {
+	double length = 10;
+	double perimeter = length * 4 ;
+	double circumference = perimeter;
+	double Radius = circumference/(2*Math.PI);
+
+	System.out.println("Radius of circular fence - " + Radius );	
+	
+	
+	
+	
+	
+	
+	}
+
+
+
+}
