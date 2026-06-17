3b5aeeb2aaa717890d335af96c24e3c2f2861828
shehan183 <IT26101226@my.sliit.lk>
2026-06-17 16:22:58 +0530

    Add files via upload

---
+++ Q1/Lab2Q1 (1).java
@@ -0,0 +1,25 @@
+public class Lab2Q1 {
+	public static void main(String[] args) {
+		double perameter = 100;
+		double length = (2 * perameter)/7;
+		double width = (3.0/4) * length;
+		
+		System.out.println("length of the fence - "+ length);
+		System.out.println("width of the fence - "+ width);
+		
+		
+	
+	
+	
+	
+	
+	}
+
+
+
+
+
+
+
+
+}
