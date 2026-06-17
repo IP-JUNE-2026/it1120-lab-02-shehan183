7b5797b82cfcf5af6a82e997a42474bac7e8e893
shehan183 <IT26101226@my.sliit.lk>
2026-06-17 16:25:16 +0530

    Add files via upload

---
+++ Q3/Lab2Q3 (1).java
@@ -0,0 +1,18 @@
+public class Lab2Q3 {
+	public static void main(String[] args) {
+	
+	double sideA = 3 ;
+	double sideB = 4 ;
+	double Hypotenuse = Math.sqrt(sideA*sideA + sideB * sideB);
+	
+	System.out.println("Length of the Hypotenuse is " + Hypotenuse);
+	
+	}
+
+
+
+
+
+
+
+}
